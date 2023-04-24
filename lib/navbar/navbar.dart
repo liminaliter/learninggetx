05f50de811274fc4e2ae5fb236c15bb2419fbcb7
liminaliter <liminaliter@gmail.com>
2023-04-24 23:02:53 +0900

    getx nevbar complete

---
+++ lib/navbar/navbar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getxbottom/controller/controller.dart';
+import 'package:getxbottom/screens/chart.dart';
+import 'package:getxbottom/screens/home.dart';
+import 'package:getxbottom/screens/setting.dart';
+import 'package:iconly/iconly.dart';
+
+import '../screens/note.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  final controller = Get.put(NavBarController());
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<NavBarController>(builder: (context) {
+      return Scaffold(
+        body: IndexedStack(
+          index: controller.tabIndex,
+          children: const [
+            Home(),
+            Note(),
+            Chart(),
+            Setting(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.amberAccent,
+          unselectedItemColor: Colors.grey.shade300,
+          currentIndex: controller.tabIndex,
+          onTap: controller.changeTapIndex,
+          items: [
+            _bottombarItem(IconlyBold.home, "Home"),
+            _bottombarItem(IconlyBold.document, "Note"),
+            _bottombarItem(IconlyBold.chart, "Chart"),
+            _bottombarItem(IconlyBold.setting, "Setting"),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+_bottombarItem(IconData icon, String label) {
+  return BottomNavigationBarItem(icon: Icon(icon), label: label);
+}
